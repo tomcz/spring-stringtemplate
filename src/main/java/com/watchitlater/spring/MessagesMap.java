131502a8813ffa4265594ff5d163bc0286a704ad
Tom Czarniecki <github@tomczarniecki.com>
2010-10-16 09:36:58 +1100

    removed need for to_s suffix in messages - messages with arguments can be created as lists inside the templates - a much more StringTemplate friendly way

--- src/main/java/com/watchitlater/spring/MessagesMap.java
+++ src/main/java/com/watchitlater/spring/MessagesMap.java
@@ -2,6 +2,9 @@ package com.watchitlater.spring;
 
 import org.springframework.web.servlet.support.RequestContext;
 
+import java.util.Collections;
+import java.util.List;
+
 public class MessagesMap extends DataMap {
 
     protected final RequestContext context;
@@ -10,11 +13,43 @@ public class MessagesMap extends DataMap {
         this.context = context;
     }
 
-    public Object get(Object key) {
-        return new MessageMap(context, (String) key);
-    }
-
     public boolean containsKey(Object key) {
         return true;
+    }
+
+    public Object get(Object key) {
+        if (key instanceof List) {
+            List list = (List) key;
+            switch (list.size()) {
+                case 0:
+                    return "";
+                case 1:
+                    return get(list.get(0));
+                default:
+                    return getMessage(list);
+            }
+        }
+        return getMessage(key.toString());
+    }
+
+    protected String getMessage(List list) {
+        String code = list.get(0).toString();
+        List args = list.subList(1, list.size());
+        String defaultMessage = defaultMessage(code);
+        return getMessage(code, args, defaultMessage);
+    }
+
+    protected String getMessage(String code) {
+        List args = Collections.emptyList();
+        String defaultMessage = defaultMessage(code);
+        return getMessage(code, args, defaultMessage);
+    }
+
+    protected String defaultMessage(String code) {
+        return "?" + code + "?";
+    }
+
+    protected String getMessage(String code, List args, String defaultMessage) {
+        return context.getMessage(code, args, defaultMessage);
     }
 }
--- src/main/java/com/watchitlater/spring/ThemeMessagesMap.java
+++ src/main/java/com/watchitlater/spring/ThemeMessagesMap.java
@@ -2,13 +2,16 @@ package com.watchitlater.spring;
 
 import org.springframework.web.servlet.support.RequestContext;
 
+import java.util.List;
+
 public class ThemeMessagesMap extends MessagesMap {
 
     public ThemeMessagesMap(RequestContext context) {
         super(context);
     }
 
-    public Object get(Object key) {
-        return new ThemeMessageMap(context, (String) key);
+    @Override
+    protected String getMessage(String code, List args, String defaultMessage) {
+        return context.getThemeMessage(code, args, defaultMessage);
     }
 }
