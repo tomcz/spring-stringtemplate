4129309453d09a8424cc4fecee690de6c51e5c21
tomcz <github@tomczarniecki.com>
2011-04-24 23:38:30 +1000

    turn off taglib configuration in jetty - let's speed it up

--- src/test/java/com/watchitlater/spring/webapp/WebServer.java
+++ src/test/java/com/watchitlater/spring/webapp/WebServer.java
@@ -3,6 +3,11 @@ package com.watchitlater.spring.webapp;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.webapp.WebAppContext;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class WebServer {
 
     private Server server;
@@ -15,11 +20,26 @@ public class WebServer {
     public WebServer start() throws Exception {
         System.setProperty("org.mortbay.xml.XmlParser.Validating", "false");
 
+        WebAppContext context = new WebAppContext("src/test/webapp", "/stringtemplate");
+        context.setConfigurationClasses(removeTagLibConfiguration(context));
+
         server = new Server(port);
-        server.addHandler(new WebAppContext("src/test/webapp", "/stringtemplate"));
+        server.addHandler(context);
         server.start();
 
         return this;
+    }
+
+    private String[] removeTagLibConfiguration(WebAppContext context) {
+        List<String> configurationClasses = new ArrayList<String>();
+        Collections.addAll(configurationClasses, context.getConfigurationClasses());
+        Iterator<String> itr = configurationClasses.iterator();
+        while (itr.hasNext()) {
+            if (itr.next().endsWith("TagLibConfiguration")) {
+                itr.remove();
+            }
+        }
+        return configurationClasses.toArray(new String[configurationClasses.size()]);
     }
 
     public void stop() {
